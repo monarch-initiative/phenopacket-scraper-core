f17ce67e78997daed3de2aa8cf09320f2174b551
satwik77 <satwik55@gmail.com>
2016-06-02 00:22:48 +0530

    CLI modifications

--- pps/main.py
+++ pps/main.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+
 
 from cliff.app import App
 from cliff.commandmanager import CommandManager
@@ -16,6 +18,68 @@ class Phenopacketscraper(App):
 
     def initialize_app(self, argv):
         self.LOG.debug('initialize_app')
+
+
+    def build_option_parser(self, description, version,
+                            argparse_kwargs=None):
+
+        argparse_kwargs = argparse_kwargs or {}
+        parser = argparse.ArgumentParser(
+            description=description,
+            add_help=False,
+            **argparse_kwargs
+        )
+        parser.add_argument(
+            '--version',
+            action='version',
+            version='%(prog)s {0}'.format(version),
+        )
+        parser.add_argument(
+            '-v', '--verbose',
+            action='count',
+            dest='verbose_level',
+            default=self.DEFAULT_VERBOSE_LEVEL,
+            help='Increase verbosity of output. Can be repeated.',
+        )
+        parser.add_argument(
+            '--log-file',
+            action='store',
+            default=None,
+            help='Specify a file to log output. Disabled by default.',
+        )
+        parser.add_argument(
+            '-q', '--quiet',
+            action='store_const',
+            dest='verbose_level',
+            const=0,
+            help='suppress output except warnings and errors',
+        )
+        if self.deferred_help:
+            parser.add_argument(
+                '-h', '--help',
+                dest='deferred_help',
+                action='store_true',
+                help="show this help message and exit",
+            )
+        else:
+            parser.add_argument(
+                '-h', '--help',
+                action=HelpAction,
+                nargs=0,
+                default=self,  # tricky
+                help="show this help message and exit",
+            )
+        parser.add_argument(
+            '--debug',
+            default=False,
+            action='store_true',
+            help='show tracebacks on errors',
+        )
+
+
+        return parser
+
+
 
     def prepare_to_run_command(self, cmd):
         self.LOG.debug('prepare_to_run_command %s', cmd.__class__.__name__)
--- pps/scraper.py
+++ pps/scraper.py
@@ -1,21 +1,32 @@
 import logging
-
+import sys
+import requests
 from cliff.command import Command
-
+from bs4 import BeautifulSoup
 
 class Scraper(Command):
-    "A simple command that prints a message."
 
     log = logging.getLogger(__name__)
 
+    def get_parser(self, prog_name):
+        parser = super(Scraper, self).get_parser(prog_name)
+        parser.add_argument('-u', '--url', type=str)
+        return parser
+
     def take_action(self, parsed_args):
-        self.log.info('test')
+        argx = sys.argv[1:]
+        self.log.info('testing')
         self.log.debug('debugging')
-        self.app.stdout.write('0\n')
+        option = parsed_args.url
+        self.app.stdout.info('Arguments: '+str(argx)+'\n')
+        self.app.stdout.info(str(option)+'\n')
+
+
+        # self.app.stdout.write(str(self.parsed_args))
+
 
 
 class Error(Command):
-    "Always raises an error"
 
     log = logging.getLogger(__name__)
 
