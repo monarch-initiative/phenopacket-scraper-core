e1412a01156c5b80d4718cfb74bccd5acf9d2614
satwik77 <satwik55@gmail.com>
2016-06-03 00:43:08 +0530

    File Output and Exception Handling

--- pps/scraper.py
+++ pps/scraper.py
@@ -16,6 +16,7 @@ class Scraper(Command):
     def get_parser(self, prog_name):
         parser = super(Scraper, self).get_parser(prog_name)
         parser.add_argument('-u', '--url', type=str)
+        parser.add_argument('-o', '--output', type=str)
         return parser
 
     def take_action(self, parsed_args):
@@ -29,23 +30,46 @@ class Scraper(Command):
         req_ob = requests.get(option)
         
         gaussian = BeautifulSoup(req_ob.content, "html.parser")
-        title = gaussian.find_all("title")[0]
-        self.app.stdout.write("Title: " + str(title.text.decode('utf-8')) + "\n\n")
-
         
-        abstract = gaussian.find_all("p", {"id": "p-2"})[0]
-        abs_text = abstract.text.encode('ascii','ignore')
-        self.app.stdout.write("Abstract:\n")
-        # self.app.stdout.write(abs_text)
-        self.app.stdout.write(abs_text)
+        try:
+            title = gaussian.find_all("title")[0]
+            self.app.stdout.write("Title: " + str(title.text.decode('utf-8')) + "\n\n")
+        except:
+            pass
+        
+        try:        
+            abstract = gaussian.find_all("p", {"id": "p-2"})[0]
+            abs_text = abstract.text.encode('ascii','ignore')
+            self.app.stdout.write("Abstract:\n")
+            # self.app.stdout.write(abs_text)
+            self.app.stdout.write(abs_text)
+
+            if parsed_args.output:
+                fopen = open( str(parsed_args.output) +'_abstract.txt','w')
+                fopen.write(abs_text)
+                fopen.close()
+
+        except:
+            self.app.stdout.write("Abstract Not found\n")
+
 
         hpo_obs = gaussian.find_all("a", {"class": "kwd-search"})
-        # self.app.stdout.write(str(hpo_obs)+'\n\n')
-        self.app.stdout.write('HPO Terms:\n')
-        for ob in hpo_obs:
-            self.app.stdout.write(ob.text)
-            self.app.stdout.write('\n')
 
+        if hpo_obs:
+            # self.app.stdout.write(str(hpo_obs)+'\n\n')
+            self.app.stdout.write('HPO Terms:\n')
+            for ob in hpo_obs:
+                self.app.stdout.write(ob.text + '\n')
+                # self.app.stdout.write('\n')
+
+            if parsed_args.output:
+                fopen = open( str(parsed_args.output) +'_hpo_terms.txt','w')
+                for ob in hpo_obs:
+                    fopen.write(ob.text + '\n')
+
+                fopen.close()
+        else:
+            self.app.stdout.write("HPO Terms Not found")
 
 
 
